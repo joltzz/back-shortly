25dcf9c21388391ec76032ef67dd7aa5b6674ee2
joltzz <guijsilva02@gmail.com>
2022-06-08 18:03:18 -0300

    feat: fixing final errors

--- src/app.js
+++ src/app.js
@@ -9,9 +9,9 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(json());
-app.use(router());
+app.use(router);
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>{
     console.log(chalk.bold.green(`Servidor funcionando na porta ${PORT}`));
 });
