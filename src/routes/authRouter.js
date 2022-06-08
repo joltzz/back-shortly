c9dc1873b825522936c3ccc9dfc111ec2cd0bd88
joltzz <guijsilva02@gmail.com>
2022-06-08 14:27:19 -0300

    feat: auth router done

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -3,4 +3,8 @@ import { login } from "../controllers/authController.js";
 import { validatesSchemaMiddleware } from "../middlewares/validateSchemaMiddleware.js";
 import loginSchema from "../schemas/loginSchema.js";
 
+const authRouter = Router();
 
+authRouter.post("/login", validatesSchemaMiddleware(loginSchema), login);
+
+export default authRouter;
