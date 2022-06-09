0cba8226cfaa14bf89bff1a2d3db1c5889b3ef3b
joltzz <guijsilva02@gmail.com>
2022-06-09 14:18:53 -0300

    feat: fixed auth router

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { newUser, login } from "../controllers/userController.js";
+import { newUser, login, getInfoUsers, getRanking } from "../controllers/userController.js";
+import { validateTokenMiddleware } from "../middlewares/validateTokenMiddleware.js";
 
 const authRouter = Router();
 
 authRouter.post("/signup", newUser);
 authRouter.post("/signin", login);
+authRouter.get("/users/:id", validateTokenMiddleware, getInfoUsers);
+authRouter.get("/ranking", getRanking);
 
 export default authRouter;
